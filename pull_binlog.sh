7806cf39bf10e28a39a8035f9444160eb5c1db49
root <root@precise32.(none)>
2014-08-28 20:18:16 +0000

    compressed backups

--- pull_binlog.sh
+++ pull_binlog.sh
@@ -7,6 +7,7 @@
 clear
 
 set -o pipefail
+
 # Initial values
 
 lockFile="/var/lock/binlog-pull.lock"
@@ -17,7 +18,7 @@ mysqlUser=root
 mysqlPort=3306
 remoteHost=192.168.1.105
 binPrefix="mysql-bin"
-backupPath="/root/"
+backupPath="/root/backups"
 respawn=3 # How many attempts to restart the mysqlbinlog process try
 email="daniel.guzman.burgos@percona.com"
 
@@ -33,7 +34,9 @@ function sendAlert () {
 
 function destructor () {
         sendAlert
-        rm -f "$lockFile" #"$errorFile"
+	local latest=$(ls -1 $backupPath | grep $binPrefix | tail -1)
+        rm -f $lockFile $errorFile $backupPath/$latest
+	
 }
 
 # Setting TRAP in order to capture SIG and cleanup things
@@ -45,7 +48,7 @@ function verifyExecution () {
         if [ $exitCode -ne "0" ]
         then
                 msg="[ERROR] Failed execution. ${2}"
-                echo "$msg" >> ${errorfile}
+                echo "$msg" >> ${errorFile}
                 if [ "$mustDie" == "true" ]; then
                         exit 1
                 else
@@ -56,9 +59,10 @@ function verifyExecution () {
 }
 
 function setLockFile () {
-        if [ -e "$lockFile" ]; then
+	pidmbl=$(pidof mysqlbinlog)
+        if [[ -e "$lockFile"  || ! -z "$pidmbl" ]]; then
                 trap - EXIT INT TERM
-                verifyExecution "1" "Script already running. $lockFile exists"
+                verifyExecution "1" "Script already running. $lockFile exists or mysqlbinlog is already running. $pidmbl"
                 sendAlert
                 rm -f "$errorFile"
                 exit 2
@@ -96,14 +100,15 @@ function findLatestBinlog () {
         verifyExecution "$?"  "Backup path $backupPath does not exists" true
 
         local latest=$(ls -1 | grep $binPrefix | tail -1)
-	msg="[OK] Found latest backupe binlog: $latest"
+	msg="[OK] Found latest backup binlog: $latest"
         if [ -z "$latest" ]; then
                 latest=$(findFirstBinlog)
 		msg="[Warning] No binlog file founded on backup directory (${backupPath}). Using instead $latest as first file"
         fi
 	logInfo "$msg"
         popd &>/dev/null
-	echo $latest
+	latest=${latest%%.gz}
+	echo "$latest"
 }
 
 function pullBinlogs () {
@@ -115,7 +120,7 @@ function pullBinlogs () {
 	out=$(mysqlbinlog --raw --read-from-remote-server --stop-never --verify-binlog-checksum --user=${mysqlUser} --host=${remoteHost} --port=${mysqlPort} --stop-never-slave-server-id=54060 $firstBinlogFile 2>&1) &
 	verifyExecution "$?"  "Error while launching mysqlbinlog utility. $out"
 	pidMysqlbinlog=$(pidof mysqlbinlog)
-	logInfo "[OK] Launched mysqlbinlog utility. Backup running"
+	logInfo "[OK] Launched mysqlbinlog utility. Backup running: mysqlbinlog --raw --read-from-remote-server --stop-never --verify-binlog-checksum --user=${mysqlUser} --host=${remoteHost} --port=${mysqlPort} --stop-never-slave-server-id=54060 $firstBinlogFile"
 
 	popd &>/dev/null
 }
@@ -123,8 +128,24 @@ function pullBinlogs () {
 function rotateBackups () {
         
 	out=$(find $backupPath -type f -name "${binPrefix}*" -mtime +30 -print | xargs /bin/rm 2>&1)
-	verifyExecution "$?" "Error while removing old backups" true
+	es=$?
+	if [ "$es" -ne 123 ]; then
+		verifyExecution "$es" "Error while removing old backups. $out" true
+	fi
 	
+}
+
+function compressBinlogs () {
+	
+	pushd $backupPath &> /dev/null
+	
+	for i in $(ls -1 | grep $binPrefix | grep -v ".gz"); do
+		out=$(gzip $i 2>&1)
+		verifyExecution "$?"  "Error compressing binlog file ${i}. $out" true
+	done
+
+	popd &>/dev/null
+
 }
 
 function verifyAllRunning () {
@@ -139,13 +160,18 @@ function verifyAllRunning () {
 				verifyExecution "1"  "Error while restarting mysqlbinlog utility after $respawn attempts. Terminating the script" true
 			fi
 		fi
+
+		compressBinlogs
 		rotateBackups
+
 		sleep 30;
 	done
 
 }
 
+setLockFile
 verifyMysqlbinlog
 pullBinlogs
 
+compressBinlogs
 verifyAllRunning
